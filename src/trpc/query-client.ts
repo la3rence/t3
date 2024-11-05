3b775d8c807cc8c8219d5aecb3093b732c297898
lawrence.li <hi@lawrenceli.me>
2024-11-05 16:56:23 +0800

    chore: react query stale

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -13,8 +13,10 @@ export const createQueryClient = () =>
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: 5 * 1000, // 自动刷新
-        refetchOnWindowFocus: "always"
+        staleTime: 10 * 1000, // 保质期
+        refetchInterval: 5 * 1000, // 自动刷新间隔
+        refetchIntervalInBackground: true, // 后台刷新
+        refetchOnWindowFocus: "always",
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
