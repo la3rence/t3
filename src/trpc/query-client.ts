89c801e0465c46ae051aca617d2645ab91d29d7e
lawrence.li <hi@lawrenceli.me>
2024-11-05 15:55:11 +0800

    chore: react query stale

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -4,13 +4,17 @@ import {
 } from "@tanstack/react-query";
 import SuperJSON from "superjson";
 
+/**
+ * React Query configuration
+ */
 export const createQueryClient = () =>
   new QueryClient({
     defaultOptions: {
       queries: {
         // With SSR, we usually want to set some default staleTime
         // above 0 to avoid refetching immediately on the client
-        staleTime: 30 * 1000,
+        staleTime: 5 * 1000, // 自动刷新
+        refetchOnWindowFocus: "always"
       },
       dehydrate: {
         serializeData: SuperJSON.serialize,
